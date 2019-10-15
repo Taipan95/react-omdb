a53ec56a622513ed2ac11102d89841bf1dba0cf5
Sakis <sakis_95@live.com>
2019-10-16 01:23:58 +0300

    Updates

---
+++ precache-manifest.3c23f7b320703c0160b43c63fc8f6d0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf6628d3b39e5136b2255112da4341f",
+    "url": "/react-omdb/index.html"
+  },
+  {
+    "revision": "906625623e3085e8aae3",
+    "url": "/react-omdb/static/css/main.1293b088.chunk.css"
+  },
+  {
+    "revision": "7b4704b939d95a95b1d2",
+    "url": "/react-omdb/static/js/2.9350592b.chunk.js"
+  },
+  {
+    "revision": "906625623e3085e8aae3",
+    "url": "/react-omdb/static/js/main.74ad9a28.chunk.js"
+  },
+  {
+    "revision": "e08d2a80d1ac9521d0fd",
+    "url": "/react-omdb/static/js/runtime-main.f21b52f5.js"
+  }
+]);
